831482072fdec3f4ee66f9a0ae2fafac5fe67e69
Charles Lambret <charleslambretpro@gmail.com>
2025-02-12 16:23:53 +0100

    rate limiter

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"hetic-cdn-project/app"
 	"log"
 	"net/http"
+	"golang.org/x/time/rate"
+	"time"
 )
 
 const PORT = ":8080"
@@ -15,8 +17,12 @@ func main() {
 	app.InitS3Client()
 	router := app.NewRouter()
 
+	// 5 requests per second with a burst of 10
+    limiter := app.NewRateLimiter(rate.Every(time.Second/5), 10)
+    limitedRouter := app.RateLimitMiddleware(limiter, router)
+
 	log.Println("Server is running on", HOST)
-	err := http.ListenAndServe(PORT, router)
+	err := http.ListenAndServe(PORT, limitedRouter)
 	if err != nil {
 		log.Fatal("Error starting server!", err)
 		return
