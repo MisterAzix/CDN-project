98ab528045fb9466d63d55699e4c2eb34092c1fe
Charles Lambret <charleslambretpro@gmail.com>
2025-02-13 15:14:42 +0100

    hotfix part 1

--- app/auth.go
+++ app/auth.go
@@ -1,11 +1,7 @@
 package app
 
 import (
-<<<<<<< HEAD
     "context"
-=======
-"context"
->>>>>>> 4ae975125751ec90717dba71fd37f4727adfe13d
     "encoding/json"
     "fmt"
     "log"
@@ -151,74 +147,4 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 
     w.WriteHeader(http.StatusOK)
     json.NewEncoder(w).Encode(map[string]string{"token": tokenString, "userID": user.ID.Hex()})
-<<<<<<< HEAD
-=======
-=======
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"go.mongodb.org/mongo-driver/bson"
-	"golang.org/x/crypto/bcrypt"
-)
-
-func registerHandler(w http.ResponseWriter, r *http.Request) {
-	var user User
-	err := json.NewDecoder(r.Body).Decode(&user)
-	if err != nil {
-		http.Error(w, "Invalid input", http.StatusBadRequest)
-		return
-	}
-
-	collection := client.Database("your-db").Collection("users")
-	var existingUser User
-	err = collection.FindOne(r.Context(), bson.M{"email": user.Email}).Decode(&existingUser)
-	if err == nil {
-		http.Error(w, "Email already registered", http.StatusConflict)
-		return
-	}
-
-	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
-	if err != nil {
-		http.Error(w, "Error hashing password", http.StatusInternalServerError)
-		return
-	}
-
-	_, err = collection.InsertOne(r.Context(), bson.M{
-		"email":    user.Email,
-		"password": hash,
-	})
-	if err != nil {
-		http.Error(w, "Error saving user", http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusCreated)
-	fmt.Fprintf(w, "User registered successfully")
-}
-
-func loginHandler(w http.ResponseWriter, r *http.Request) {
-	var user User
-	err := json.NewDecoder(r.Body).Decode(&user)
-	if err != nil {
-		http.Error(w, "Invalid input", http.StatusBadRequest)
-		return
-	}
-
-	collection := client.Database("your-db").Collection("users")
-	var existingUser User
-	err = collection.FindOne(r.Context(), bson.M{"email": user.Email}).Decode(&existingUser)
-	if err != nil {
-		http.Error(w, "Email not found", http.StatusUnauthorized)
-		return
-	}
-
-	err = bcrypt.CompareHashAndPassword([]byte(existingUser.Password), []byte(user.Password))
-	if err != nil {
-		http.Error(w, "Invalid password", http.StatusUnauthorized)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Login successful")
->>>>>>> 4ae975125751ec90717dba71fd37f4727adfe13d
 }
