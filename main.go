448450025da781c9fddae1b1ed4b524f55cb7643
DFarau <44142390+DFarau@users.noreply.github.com>
2025-02-13 14:49:44 +0100

    feath(app): added login and register (#72)

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"hetic-cdn-project/app"
 	"log"
 	"net/http"
+	"github.com/rs/cors"
 )
 
 const PORT = ":8080"
@@ -14,9 +15,18 @@ func main() {
 	app.ConnectDB()
 	app.InitS3Client()
 	router := app.NewRouter()
+
+	corsHandler := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},
+		AllowCredentials: true,
+	})
+
+	handler := corsHandler.Handler(router)
 	
 	log.Println("Server is running on", HOST)
-	err := http.ListenAndServe(PORT, router)
+	err := http.ListenAndServe(PORT, handler)
 	if err != nil {
 		log.Fatal("Error starting server!", err)
 		return
