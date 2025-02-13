8a1897f66b17df2571f3efe08702462dd5042744
MisterAzix <flizi@live.fr>
2025-02-13 15:41:08 +0100

    chore: adding k8s configuration 🔧

--- app/env.go
+++ app/env.go
@@ -3,9 +3,14 @@ package app
 import (
 	"github.com/joho/godotenv"
 	"log"
+	"os"
 )
 
 func LoadEnv() {
+	if os.Getenv("ENV") == "production" {
+		log.Println("Running in production mode!")
+		return
+	}
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
